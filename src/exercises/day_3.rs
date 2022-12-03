01833982c009b4e3f9c1c37ece587c907f795c7b
Brian Koehler <briandkoehler@gmail.com>
2022-12-03 01:01:16 -0800

    Implement uniques from string fn

--- src/exercises/day_3.rs
+++ src/exercises/day_3.rs
@@ -4,6 +4,11 @@ use crate::utils::{Solution, read_lines};
 const LOWERCASE_OFFSET: u32 = 96;
 const UPPERCASE_OFFSET: u32 = 65 - 27;
 
+fn get_uniques_from_str<S: AsRef<str>>(stringlike: S) -> HashSet<char> {
+    let items: Vec<_> = stringlike.as_ref().chars().collect();
+    HashSet::from_iter(items)
+}
+
 pub struct Day3 {
     input_path: String
 }
@@ -23,10 +28,8 @@ impl Solution for Day3 {
             let compartment_1 = &rucksack[..rucksack.len() / 2];
             let compartment_2 = &rucksack[rucksack.len() / 2..];
 
-            let mut items: Vec<_> = compartment_1.chars().collect();
-            let uniques_1: HashSet<_> = HashSet::from_iter(items);
-            items = compartment_2.chars().collect();
-            let uniques_2: HashSet<_> = HashSet::from_iter(items);
+            let uniques_1 = get_uniques_from_str(compartment_1);
+            let uniques_2 = get_uniques_from_str(compartment_2);
             
             let common: Vec<_> = uniques_1.intersection(&uniques_2).collect();
             let item = common[0];
@@ -44,12 +47,9 @@ impl Solution for Day3 {
 
         let mut priority_sum = 0;
         for group in lines.windows(3).step_by(3) {
-            let mut items: Vec<_> = group[0].chars().collect();
-            let uniques_1: HashSet<_> = HashSet::from_iter(items);
-            items = group[1].chars().collect();
-            let uniques_2: HashSet<_> = HashSet::from_iter(items);
-            items = group[2].chars().collect();
-            let uniques_3: HashSet<_> = HashSet::from_iter(items);
+            let uniques_1 = get_uniques_from_str(group[0].as_str());
+            let uniques_2 = get_uniques_from_str(group[1].as_str());
+            let uniques_3 = get_uniques_from_str(group[2].as_str());
 
             let commons: Vec<_> = uniques_1.iter()
                 .filter(|item| uniques_2.contains(item))
