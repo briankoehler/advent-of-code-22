f6e55e82bee61ce0066e3997ea51196fdd35e0c9
Brian Koehler <briandkoehler@gmail.com>
2022-12-04 01:01:42 -0800

    Add day 4

---
+++ src/exercises/day_4.rs
@@ -0,0 +1,67 @@
+use std::str::FromStr;
+use crate::utils::{Solution, read_lines};
+
+struct Assignment {
+    pub lower: u32,
+    pub upper: u32,
+}
+
+impl FromStr for Assignment {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let tokens: Vec<_> = s.split("-").collect();
+        return Ok(Self {
+            lower: tokens[0].parse::<u32>().expect("Invalid assignment"),
+            upper: tokens[1].parse::<u32>().expect("Invalid assignment"),
+        })
+    }
+}
+
+pub struct Day4 {
+    input_path: String,
+}
+
+impl Solution for Day4 {
+    fn new(input_path: String) -> Self {
+        Self { input_path }
+    }
+
+    fn part_1(&self) -> Result<u64, Box<dyn std::error::Error>> {
+        let lines = read_lines(&self.input_path)?;
+
+        let mut count = 0;
+        for line in lines {
+            let line = line?;
+            let pairs: Vec<_> = line.split(",").collect();
+            let assignment_1 = Assignment::from_str(pairs[0])?;
+            let assignment_2 = Assignment::from_str(pairs[1])?;
+
+            if (assignment_1.lower <= assignment_2.lower && assignment_1.upper >= assignment_2.upper)
+                || (assignment_2.lower <= assignment_1.lower && assignment_2.upper >= assignment_1.upper) {
+                count += 1;
+            }
+        }
+
+        Ok(count)
+    }
+
+    fn part_2(&self) -> Result<u64, Box<dyn std::error::Error>> {
+        let lines = read_lines(&self.input_path)?;
+
+        let mut count = 0;
+        for line in lines {
+            let line = line?;
+            let pairs: Vec<_> = line.split(",").collect();
+            let assignment_1 = Assignment::from_str(pairs[0])?;
+            let assignment_2 = Assignment::from_str(pairs[1])?;
+
+            if (assignment_2.lower <= assignment_1.lower && assignment_1.lower <= assignment_2.upper)
+                || (assignment_1.lower <= assignment_2.lower && assignment_2.lower <= assignment_1.upper) {
+                count += 1;
+            }
+        }
+
+        Ok(count)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod exercises;
 mod utils;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let solution = exercises::day_3::Day3::new(String::from("./inputs/day_3.txt"));
+    let solution = exercises::day_4::Day4::new(String::from("./inputs/day_4.txt"));
     println!("Part 1 Answer: {}", solution.part_1()?);
     println!("Part 2 Answer: {}", solution.part_2()?);
     Ok(())
